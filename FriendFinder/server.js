acf62c86ff7b8de238e66f97aece6cf859152fce
thescottbob <scottmcmains@gmail.com>
2018-11-08 15:37:35 -0800

    Ready for Deployment to Heroku

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -6,14 +6,16 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var apiRoutes = ;
 
 app.get('/', function (req, res) {
     res.send('Hello World')
   })
    
-  app.listen(3000)
+  app.listen(3000, function(){
+    console.log("Listening on PORT: " + PORT);
+  })
 
   
 
