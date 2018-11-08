6b3d9d7cafaa2b47e750b092982c408a923f9160
thescottbob <scottmcmains@gmail.com>
2018-11-08 13:40:15 -0800

    Improved Comments

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -3,3 +3,13 @@
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
 
+app.get('/app/public/survey', function (req, res) {
+    res.send('Hello World')
+  })
+
+
+// if/else statement?
+
+  app.get('/home.html', function (req, res) {
+    res.send('Hello World')
+  })
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,6 +1,13 @@
-var express = require('express');
+  // Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
 var app = express();
-const path = require('path');
+var PORT = 3000;
+var apiRoutes = ;
 
 app.get('/', function (req, res) {
     res.send('Hello World')
@@ -8,23 +15,51 @@ app.get('/', function (req, res) {
    
   app.listen(3000)
 
-  // You should save your application's data inside of app/data/friends.js as an array of objects.
-  // Each of these objects should roughly follow the format below.
+  
 
-  // {
-  //   "name":"Ahmed",
-  //   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  //   "scores":[
-  //       5,
-  //       1,
-  //       4,
-  //       4,
-  //       5,
-  //       1,
-  //       2,
-  //       5,
-  //       4,
-  //       1
-  //     ]
-  // }
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var tables = [
+    {
+        customerName: "Ahmed",
+        phoneNumber: "979-587-0887",
+        customerEmail: "afhaque89@gmail.com",
+        customerID: "afhaque89"
+    },
+    {
+        customerName: "test2",
+        phoneNumber: "979-587-0887",
+        customerEmail: "afhaque89@gmail.com",
+        customerID: "afhaque89"
+    },
+];
+var waitlist = [];
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
+
+app.get('/tables', function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get('/reserve', function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
   
