f6033168f6695c55476d402d20b15a13d8c38fee
thescottbob <scottmcmains@gmail.com>
2018-11-09 18:40:18 -0800

    Added Comments

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -1,12 +1,18 @@
-  module.exports = function(app) {
-      
+$(document).ready(function(){
+  
+  $("#begin").on("click", function(app){
+
     // A GET Route to /survey which should display the survey page.
     app.get("/survey.html", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-  
+
     // A default, catch-all route that leads to home.html which displays the home page.
     app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-  };
+
+    })
+});
+  
+  // module.exports = function(app) {};
